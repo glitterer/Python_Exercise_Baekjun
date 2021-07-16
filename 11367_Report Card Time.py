09bc0265eaabea200f774229c16119d62b7d4dcc
glitterer <50728605+glitterer@users.noreply.github.com>
2021-07-16 14:40:10 +0900

    Update 11367_Report Card Time.py

--- 11367_Report Card Time.py
+++ 11367_Report Card Time.py
@@ -21,3 +21,30 @@ for i in range(0, repeat):
     else:
         grade = "F"
     print(name, grade, sep=' ')
+
+ '''
+    repeat = int(input())
+for i in range(0, repeat):
+    name, score = input().split()  # 이 부분이 중요. 2개 이상 입력받기를 계속 햇갈리는 중...2021-7-16
+    # print(name, score, sep=' ')
+    score = int(score)  # 이렇게 하나만 넣으면 score는 int형으로 변환이 된다.
+    if score >= 97:
+        grade = "A+"
+    elif score <= 96 and score >= 90:
+        grade = "A"
+    elif score <= 89 and score >= 87:
+        grade = "B+"
+    elif score <= 86 and score >= 80:
+        grade = "B"
+    elif score <= 79 and score >= 77:
+        grade = "C+"
+    elif score <= 76 and score >= 70:
+        grade = "C"
+    elif score <= 69 and score >= 67:
+        grade = "D+"
+    elif score <= 66 and score >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+    print(name, grade, sep=' ')
+'''
