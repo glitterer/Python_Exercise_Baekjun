c42e110b2cb54e0eebc5f8f4295e63b8fd8244a3
glitterer <50728605+glitterer@users.noreply.github.com>
2021-07-17 22:58:03 +0900

    Create 17010_Time to Decompress.py

---
+++ 17010_Time to Decompress.py
@@ -0,0 +1,13 @@
+for i in range(int(input())):
+    num, char = input().split()
+    for j in range(int(num)):
+        print(char, end='')
+    print()
+
+'''
+T = int(input())
+
+for i in range(T):
+    N, C = input().split()
+    print(C * int(N))
+'''
