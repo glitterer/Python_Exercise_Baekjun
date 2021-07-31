9fcd1c9c451b25a35edcf565453f815ddf9e4af4
glitterer <50728605+glitterer@users.noreply.github.com>
2021-07-31 23:07:24 +0900

    Create 10809_알파벳 찾기.py

---
+++ 10809_알파벳 찾기.py
@@ -0,0 +1,21 @@
+word = list(input())
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+index = []
+for i in range(len(list(alphabet))):
+    index.append(-1)
+
+pos = 0
+#알파벳 위치 찾기
+for i in word:  #baek
+    cnt = 0
+    for j in list(alphabet):
+        if j == i and index[cnt] != -1:
+            pos += 1
+            pass
+        elif j == i:
+            index[cnt] = pos
+            pos += 1
+        cnt += 1
+
+for i in range(len(list(alphabet))):
+    print(index[i], end = ' ')
