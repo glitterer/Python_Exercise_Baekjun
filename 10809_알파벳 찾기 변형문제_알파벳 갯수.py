1264d1c81b16bf69c4917110be2cd34c50c7124c
glitterer <50728605+glitterer@users.noreply.github.com>
2021-07-31 22:56:32 +0900

    Update and rename [미완성]10809_알파벳 찾기.py to 10809_알파벳 찾기 변형문제_알파벳 갯수.py

--- 10809_알파벳 찾기 변형문제_알파벳 갯수.py
+++ 10809_알파벳 찾기 변형문제_알파벳 갯수.py
@@ -11,8 +11,6 @@ for i in word:  #baek
     for j in list(alphabet):
         if i == j:
             index[cnt] += 1
-        elif i != j and index[cnt] != 0:  # 여기가 뭔가 문제가 있음 
-            index[cnt] = -1
         cnt += 1
 
 for i in range(len(list(alphabet))):
