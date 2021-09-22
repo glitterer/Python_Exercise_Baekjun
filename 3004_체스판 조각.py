321eec364ffebe19c44ee443caa08c4d1e56fcdb
Seik Oh <50728605+glitterer@users.noreply.github.com>
2021-09-22 23:11:05 +0900

    Create 3004_체스판 조각.py

---
+++ 3004_체스판 조각.py
@@ -0,0 +1,12 @@
+res = 1
+num = 1
+
+N = int(input())
+for i in range(N):
+    res+=num
+    if i%2==0:
+        num+=1
+
+print(res)
+
+#참고: https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-3004%EB%B2%88-%EC%B2%B4%EC%8A%A4%ED%8C%90-%EC%A1%B0%EA%B0%81-%ED%8C%8C%EC%9D%B4%EC%8D%ACPython
